81794e197d6784ea9c43be7214672a1b02dc1087
mvodolagin <m.vodolagin@artsofte.ru>
2023-05-27 12:59:47 +0500

    доработал ответы у запросов

--- SchoolMathGamesDjango/SchoolMathGamesApp/views.py
+++ SchoolMathGamesDjango/SchoolMathGamesApp/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 from .models import *
 
 
@@ -24,9 +25,17 @@ class CreateGame(APIView):
     @staticmethod
     def post(request):
         name = request.POST.get('name', '')
+        if name == '':
+            return Response({'error': 'Название игры отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
         gameType = request.POST.get('gameType', -1)
+        if gameType == -1:
+            return Response({'error': 'Тип игры отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
         start = request.POST.get('name', -1)
+        if start == -1:
+            return Response({'error': 'Время начала игры отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
         timeGame = request.POST.get('timeGame', -1)
+        if timeGame == -1:
+            return Response({'error': 'Продолжительность игры отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
         return Response(GameAllInfoMock.gameAllInfo)
 
 
@@ -34,7 +43,12 @@ class GetGameById(APIView):
     @staticmethod
     def get(request):
         gameId = request.POST.get('gameId', -1)
-        return Response(GameAllInfoMock.gameAllInfo)
+        if gameId == -1:
+            return Response({'error': 'Идентификатор игры отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
+        for game in GamesMock.gameList:
+            if gameId == game['id']:
+                return Response(GameAllInfoMock.gameAllInfo)
+        return Response({'error': 'Игра не найдена!'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UpdateGameInfo(APIView):
@@ -43,10 +57,29 @@ class UpdateGameInfo(APIView):
     @staticmethod
     def post(request):
         gameId = request.POST.get('gameId', -1)
+        if gameId == -1:
+            return Response({'error': 'Идентификатор игры отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        target_game = ''
+        for game in GamesMock.gameList:
+            if gameId == game['id']:
+                target_game = GameAllInfoMock.gameAllInfo
+                break
+        if target_game == '':
+            Response({'error': 'Игра не найдена!'}, status=status.HTTP_400_BAD_REQUEST)
+
         name = request.POST.get('name', '')
+        if not name == '':
+            target_game['name'] = name
         gameType = request.POST.get('gameType', -1)
+        if not gameType == -1:
+            target_game['gameType'] = gameType
         start = request.POST.get('name', -1)
+        if not start == -1:
+            target_game['start'] = start
         timeGame = request.POST.get('timeGame', -1)
+        if not timeGame == -1:
+            target_game['timeGame'] = timeGame
         return Response()
 
 
@@ -55,8 +88,21 @@ class AddTeam(APIView):
 
     @staticmethod
     def post(request):
-        name = request.POST.get('name', '')
         gameId = request.POST.get('gameId', -1)
+        if gameId == -1:
+            return Response({'error': 'Идентификатор игры отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        target_game = ''
+        for game in GamesMock.gameList:
+            if gameId == game['id']:
+                target_game = GameAllInfoMock.gameAllInfo
+                break
+        if target_game == '':
+            return Response({'error': 'Игра не найдена!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        name = request.POST.get('name', '')
+        if name == '':
+            return Response({'error': 'Название команды отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
         return Response()
 
 
@@ -65,9 +111,33 @@ class UpdateTeam(APIView):
 
     @staticmethod
     def post(request):
-        name = request.POST.get('name', '')
         gameId = request.POST.get('gameId', -1)
-        teamId = request.POST.get('teamId', -1)
+        if gameId == -1:
+            return Response({'error': 'Идентификатор игры отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        target_game = ''
+        for game in GamesMock.gameList:
+            if gameId == game['id']:
+                target_game = GameAllInfoMock.gameAllInfo
+                break
+        if target_game == '':
+            return Response({'error': 'Игра не найдена!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Костыль для постмана, если ты будешь нормально отправлять числа, такого быть не должно, но надо будет потестить
+        teamId = int(request.POST.get('teamId', -1))
+        if teamId == -1:
+            return Response({'error': 'Идентификатор команды отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
+        target_team = ''
+        for team in target_game['teams']:
+            if team['id'] == teamId:
+                target_team = team
+                break
+        if target_team == '':
+            return Response({'error': 'Команда не найдена!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        name = request.POST.get('name', '')
+        if name == '':
+            return Response({'error': 'Новое название команды отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
         return Response()
 
 
@@ -76,6 +146,23 @@ class UpdateGameStatus(APIView):
 
     @staticmethod
     def post(request):
+        gameId = request.POST.get('gameId', -1)
+        if gameId == -1:
+            return Response({'error': 'Идентификатор игры отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        target_game = ''
+        for game in GamesMock.gameList:
+            if gameId == game['id']:
+                target_game = GameAllInfoMock.gameAllInfo
+                break
+        if target_game == '':
+            return Response({'error': 'Игра не найдена!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        game_status = request.POST.get('status', -1)
+        if game_status == -1:
+            return Response({'error': 'Новый статус игры отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        target_game['status'] = game_status
         return Response()
 
 
@@ -85,6 +172,17 @@ class DeleteGame(APIView):
     @staticmethod
     def post(request):
         gameId = request.POST.get('gameId', -1)
+        if gameId == -1:
+            return Response({'error': 'Идентификатор игры отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        target_game = ''
+        for game in GamesMock.gameList:
+            if gameId == game['id']:
+                target_game = GameAllInfoMock.gameAllInfo
+                break
+        if target_game == '':
+            return Response({'error': 'Игра не найдена!'}, status=status.HTTP_400_BAD_REQUEST)
+
         return Response()
 
 
@@ -94,5 +192,26 @@ class DeleteTeam(APIView):
     @staticmethod
     def post(request):
         gameId = request.POST.get('gameId', -1)
-        teamId = request.POST.get('teamId', -1)
+        if gameId == -1:
+            return Response({'error': 'Идентификатор игры отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        target_game = ''
+        for game in GamesMock.gameList:
+            if gameId == game['id']:
+                target_game = GameAllInfoMock.gameAllInfo
+                break
+        if target_game == '':
+            return Response({'error': 'Игра не найдена!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Костыль для постмана, если ты будешь нормально отправлять числа, такого быть не должно, но надо будет потестить
+        teamId = int(request.POST.get('teamId', -1))
+        if teamId == -1:
+            return Response({'error': 'Идентификатор команды отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
+        target_team = ''
+        for team in target_game['teams']:
+            if team['id'] == teamId:
+                target_team = team
+                break
+        if target_team == '':
+            return Response({'error': 'Команда не найдена!'}, status=status.HTTP_400_BAD_REQUEST)
         return Response()
