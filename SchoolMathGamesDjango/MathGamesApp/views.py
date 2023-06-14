986e9526148b813269db8f3a79944425edd83a67
mvodolagin <m.vodolagin@artsofte.ru>
2023-06-14 16:04:45 +0500

    поправил подсчет суммы, поправил дефолтные значения

--- SchoolMathGamesDjango/MathGamesApp/views.py
+++ SchoolMathGamesDjango/MathGamesApp/views.py
@@ -81,27 +81,40 @@ class GetGameById(APIView):
             teams = []
             if game.type == 0:
                 for team in AbakaTeam.objects.filter(game=game):
-                    team_resp = {'teamId': team.team_id, 'name': team.team_name,
-                                 'scores': list(GamePointsSerializer(team).data.values())}
-                    team_resp['sumScore'] = sum(team_resp['scores'])
+                    team_resp = {'teamId': team.team_id, 'name': team.team_name}
+                    scores = list(GamePointsSerializer(team).data.values())
+                    sum_score = 0
+                    for i in range(1, 31):
+                        if scores[i - 1] >= 0:
+                            sum_score += abaka_dict[str(i)][scores[i - 1]]
+                    team_resp['scores'] = scores
+                    team_resp['sumScore'] = sum_score
                     teams.append(team_resp)
             elif game.type == 1:
                 for team in BonusTeam.objects.filter(game=game):
-                    team_resp = {'teamId': team.team_id, 'name': team.team_name,
-                                 'scores': list(GamePointsSerializer(team).data.values())}
-                    team_resp['sumScore'] = sum(team_resp['scores'])
+                    team_resp = {'teamId': team.team_id, 'name': team.team_name}
+                    scores = list(GamePointsSerializer(team).data.values())
+                    sum_score = 0
+                    for i in range(1, 25):
+                        if scores[i - 1] >= 0:
+                            sum_score += bonus_dict[str(i)][scores[i - 1]]
+                    team_resp['scores'] = scores
+                    team_resp['sumScore'] = sum_score
                     teams.append(team_resp)
             else:
                 for team in DominoTeam.objects.filter(game=game):
-                    team_resp = {'teamId': team.team_id, 'name': team.team_name, 'sumScore': 0,
-                                 'scores': list(GamePointsSerializerDomino(team).data.values())}
-                    for i in range(len(team_resp['scores'])):
-                        if team_resp['scores'][i] <= 0:
-                            team_resp['sumScore'] += i
+                    team_resp = {'teamId': team.team_id, 'name': team.team_name}
+                    scores = list(GamePointsSerializerDomino(team).data.values())
+                    sum_score = 0
+                    for i in range(0, 28):
+                        if scores[i] <= 0:
+                            sum_score += domino_dict[str(i)][abs(scores[i])]
+                    team_resp['scores'] = scores
+                    team_resp['sumScore'] = sum_score
                     teams.append(team_resp)
 
             result = {'id': game.game_id, 'name': game.game_name, 'status': game.status, 'type': game.type,
-                             'start': game.start, 'time': game.duration, 'teams': teams}
+                      'start': game.start, 'time': game.duration, 'teams': teams}
 
             if game.status == 0:
                 return Response(result)
@@ -147,23 +160,36 @@ class UpdateGameInfo(APIView):
         teams = []
         if target_game.type == 0:
             for team in AbakaTeam.objects.filter(game=target_game):
-                team_resp = {'teamId': team.team_id, 'name': team.team_name,
-                             'scores': list(GamePointsSerializer(team).data.values())}
-                team_resp['sumScore'] = sum(team_resp['scores'])
+                team_resp = {'teamId': team.team_id, 'name': team.team_name}
+                scores = list(GamePointsSerializer(team).data.values())
+                sum_score = 0
+                for i in range(1, 31):
+                    if scores[i - 1] >= 0:
+                        sum_score += abaka_dict[str(i)][scores[i - 1]]
+                team_resp['scores'] = scores
+                team_resp['sumScore'] = sum_score
                 teams.append(team_resp)
         elif target_game.type == 1:
             for team in BonusTeam.objects.filter(game=target_game):
-                team_resp = {'teamId': team.team_id, 'name': team.team_name,
-                             'scores': list(GamePointsSerializer(team).data.values())}
-                team_resp['sumScore'] = sum(team_resp['scores'])
+                team_resp = {'teamId': team.team_id, 'name': team.team_name}
+                scores = list(GamePointsSerializer(team).data.values())
+                sum_score = 0
+                for i in range(1, 25):
+                    if scores[i - 1] >= 0:
+                        sum_score += bonus_dict[str(i)][scores[i - 1]]
+                team_resp['scores'] = scores
+                team_resp['sumScore'] = sum_score
                 teams.append(team_resp)
         else:
             for team in DominoTeam.objects.filter(game=target_game):
-                team_resp = {'teamId': team.team_id, 'name': team.team_name, 'sumScore': 0,
-                             'scores': list(GamePointsSerializerDomino(team).data.values())}
-                for i in range(len(team_resp['scores'])):
-                    if team_resp['scores'][i] == 0:
-                        team_resp['sumScore'] += i
+                team_resp = {'teamId': team.team_id, 'name': team.team_name}
+                scores = list(GamePointsSerializerDomino(team).data.values())
+                sum_score = 0
+                for i in range(0, 28):
+                    if scores[i] <= 0:
+                        sum_score += domino_dict[str(i)][abs(scores[i])]
+                team_resp['scores'] = scores
+                team_resp['sumScore'] = sum_score
                 teams.append(team_resp)
 
         return Response({'id': target_game.game_id, 'name': target_game.game_name, 'status': target_game.status,
@@ -208,12 +234,7 @@ class AddTeam(APIView):
         else:
             team_resp['scores'] = list(GamePointsSerializer(team).data.values())
 
-        if target_game.type == 2:
-            for i in range(len(team_resp['scores'])):
-                if team_resp['scores'][i] == 0:
-                    team_resp['sumScore'] += i
-        else:
-            team_resp['sumScore'] = sum(team_resp['scores'])
+        team_resp['sumScore'] = 0
 
         return Response(team_resp)
 
@@ -261,18 +282,29 @@ class UpdateTeam(APIView):
         team.save()
 
         team_resp = {'teamId': team.team_id, 'name': team.team_name}
+        sum_score = 0
 
-        if target_game.type == 2:
-            team_resp['scores'] = list(GamePointsSerializerDomino(team).data.values())
+        if target_game.type == 0:
+            scores = list(GamePointsSerializer(team).data.values())
+            for i in range(1, 31):
+                if scores[i - 1] >= 0:
+                    sum_score += abaka_dict[str(i)][scores[i - 1]]
+            team_resp['scores'] = scores
+            team_resp['sumScore'] = sum_score
+        elif target_game.type == 1:
+            scores = list(GamePointsSerializer(team).data.values())
+            for i in range(1, 25):
+                if scores[i - 1] >= 0:
+                    sum_score += bonus_dict[str(i)][scores[i - 1]]
+            team_resp['scores'] = scores
+            team_resp['sumScore'] = sum_score
         else:
-            team_resp['scores'] = list(GamePointsSerializer(team).data.values())
-
-        if target_game.type == 2:
-            for i in range(len(team_resp['scores'])):
-                if team_resp['scores'][i] == 0:
-                    team_resp['sumScore'] += i
-        else:
-            team_resp['sumScore'] = sum(team_resp['scores'])
+            scores = list(GamePointsSerializerDomino(team).data.values())
+            for i in range(0, 28):
+                if scores[i] <= 0:
+                    sum_score += domino_dict[str(i)][abs(scores[i])]
+            team_resp['scores'] = scores
+            team_resp['sumScore'] = sum_score
 
         return Response(team_resp)
 
@@ -295,7 +327,8 @@ class UpdateGameStatus(APIView):
             return Response({'error': 'Новый статус игры отсутствует!'}, status=status.HTTP_400_BAD_REQUEST)
 
         if game_status == 1 and target_game.status != 0:
-            target_game.duration = target_game.duration + (int(datetime.datetime.now().timestamp()) - target_game.end_time)
+            target_game.duration = target_game.duration + (
+                    int(datetime.datetime.now().timestamp()) - target_game.end_time)
 
         if game_status == 2 and target_game.status == 1:
             target_game.end_time = int(datetime.datetime.now().timestamp())
@@ -312,31 +345,45 @@ class UpdateGameStatus(APIView):
         teams = []
         if target_game.type == 0:
             for team in AbakaTeam.objects.filter(game=target_game):
-                team_resp = {'teamId': team.team_id, 'name': team.team_name,
-                             'scores': list(GamePointsSerializer(team).data.values())}
-                team_resp['sumScore'] = sum(team_resp['scores'])
+                team_resp = {'teamId': team.team_id, 'name': team.team_name}
+                scores = list(GamePointsSerializer(team).data.values())
+                sum_score = 0
+                for i in range(1, 31):
+                    if scores[i - 1] >= 0:
+                        sum_score += abaka_dict[str(i)][scores[i - 1]]
+                team_resp['scores'] = scores
+                team_resp['sumScore'] = sum_score
                 teams.append(team_resp)
         elif target_game.type == 1:
             for team in BonusTeam.objects.filter(game=target_game):
-                team_resp = {'teamId': team.team_id, 'name': team.team_name,
-                             'scores': list(GamePointsSerializer(team).data.values())}
-                team_resp['sumScore'] = sum(team_resp['scores'])
+                team_resp = {'teamId': team.team_id, 'name': team.team_name}
+                scores = list(GamePointsSerializer(team).data.values())
+                sum_score = 0
+                for i in range(1, 25):
+                    if scores[i - 1] >= 0:
+                        sum_score += bonus_dict[str(i)][scores[i - 1]]
+                team_resp['scores'] = scores
+                team_resp['sumScore'] = sum_score
                 teams.append(team_resp)
         else:
             for team in DominoTeam.objects.filter(game=target_game):
-                team_resp = {'teamId': team.team_id, 'name': team.team_name, 'sumScore': 0,
-                             'scores': list(GamePointsSerializerDomino(team).data.values())}
-                for i in range(len(team_resp['scores'])):
-                    if team_resp['scores'][i] == 0:
-                        team_resp['sumScore'] += i
+                team_resp = {'teamId': team.team_id, 'name': team.team_name}
+                scores = list(GamePointsSerializerDomino(team).data.values())
+                sum_score = 0
+                for i in range(0, 28):
+                    if scores[i] <= 0:
+                        sum_score += domino_dict[str(i)][abs(scores[i])]
+                team_resp['scores'] = scores
+                team_resp['sumScore'] = sum_score
                 teams.append(team_resp)
 
         result = {'id': target_game.game_id, 'name': target_game.game_name, 'status': target_game.status,
-                         'type': target_game.type, 'start': target_game.start, 'time': target_game.duration,
-                         'teams': teams}
+                  'type': target_game.type, 'start': target_game.start, 'time': target_game.duration,
+                  'teams': teams}
 
         if target_game.status == 1:
-            result['time'] = (target_game.start // 1000 - int(datetime.datetime.now().timestamp())) + target_game.duration
+            result['time'] = (target_game.start // 1000 - int(
+                datetime.datetime.now().timestamp())) + target_game.duration
         else:
             result['time'] = (target_game.start // 1000 - target_game.end_time) + target_game.duration
 
@@ -397,18 +444,29 @@ class DeleteTeam(APIView):
                 return Response({'error': 'Команда не найдена!'}, status=status.HTTP_400_BAD_REQUEST)
 
         team_resp = {'teamId': target_team.team_id, 'name': target_team.team_name}
+        sum_score = 0
 
-        if target_game.type == 2:
-            team_resp['scores'] = list(GamePointsSerializerDomino(target_team).data.values())
+        if target_game.type == 0:
+            scores = list(GamePointsSerializer(target_team).data.values())
+            for i in range(1, 31):
+                if scores[i - 1] >= 0:
+                    sum_score += abaka_dict[str(i)][scores[i - 1]]
+            team_resp['scores'] = scores
+            team_resp['sumScore'] = sum_score
+        elif target_game.type == 1:
+            scores = list(GamePointsSerializer(target_team).data.values())
+            for i in range(1, 25):
+                if scores[i - 1] >= 0:
+                    sum_score += bonus_dict[str(i)][scores[i - 1]]
+            team_resp['scores'] = scores
+            team_resp['sumScore'] = sum_score
         else:
-            team_resp['scores'] = list(GamePointsSerializer(target_team).data.values())
-
-        if target_game.type == 2:
-            for i in range(len(team_resp['scores'])):
-                if team_resp['scores'][i] == 0:
-                    team_resp['sumScore'] += i
-        else:
-            team_resp['sumScore'] = sum(team_resp['scores'])
+            scores = list(GamePointsSerializerDomino(target_team).data.values())
+            for i in range(0, 28):
+                if scores[i] <= 0:
+                    sum_score += domino_dict[str(i)][abs(scores[i])]
+            team_resp['scores'] = scores
+            team_resp['sumScore'] = sum_score
 
         target_team.delete()
         return Response(team_resp)
@@ -440,37 +498,104 @@ class ChangeScores(APIView):
         for score in changeScores:
             print(score)
             team = team_set.get(team_id=int(score['teamId']))
-            team.update_field(score['exercise'], score['value'])
+            if target_game.type == 0:
+                exercise = int(score['exercise']) + 1
+                score['exercise'] = str(exercise)
+                team.update_field(exercise, abaka_dict[score['exercise'][int(score['value'])]])
+            elif target_game.type == 1:
+                exercise = int(score['exercise']) + 1
+                score['exercise'] = str(exercise)
+                team.update_field(exercise, bonus_dict[score['exercise'][int(score['value'])]])
+            else:
+                exercise = int(score['exercise'])
+                score['exercise'] = str(exercise)
+                team.update_field(exercise, domino_dict[score['exercise'][int(score['value'])]])
             team.save()
 
         return Response()
 
 
-domino_dict = {'point00': [0, 10],
-               'point01': [0, 1, 1],
-               'point02': [0, 2, 2],
-               'point03': [0, 3, 3],
-               'point04': [0, 4, 4],
-               'point05': [0, 5, 5],
-               'point06': [0, 6, 6],
-               'point07': [-1, 2, 1],
-               'point08': [-1, 3, 2],
-               'point09': [-1, 4, 3],
-               'point10': [-1, 5, 4],
-               'point11': [-1, 6, 5],
-               'point12': [-1, 7, 6],
-               'point13': [-2, 4, 2],
-               'point14': [-2, 5, 3],
-               'point15': [-2, 6, 4],
-               'point16': [-2, 7, 5],
-               'point17': [-2, 8, 6],
-               'point18': [-3, 6, 3],
-               'point19': [-3, 7, 4],
-               'point20': [-3, 8, 5],
-               'point21': [-3, 9, 6],
-               'point22': [-4, 8, 4],
-               'point23': [-4, 9, 5],
-               'point24': [-4, 10, 6],
-               'point25': [-5, 10, 5],
-               'point26': [-5, 11, 6],
-               'point27': [-6, 12, 6]}
+domino_dict = {'0': [0, 10],
+               '1': [0, 1, 1],
+               '2': [0, 2, 2],
+               '3': [0, 3, 3],
+               '4': [0, 4, 4],
+               '5': [0, 5, 5],
+               '6': [0, 6, 6],
+               '7': [-1, 2, 1],
+               '8': [-1, 3, 2],
+               '9': [-1, 4, 3],
+               '10': [-1, 5, 4],
+               '11': [-1, 6, 5],
+               '12': [-1, 7, 6],
+               '13': [-2, 4, 2],
+               '14': [-2, 5, 3],
+               '15': [-2, 6, 4],
+               '16': [-2, 7, 5],
+               '17': [-2, 8, 6],
+               '18': [-3, 6, 3],
+               '19': [-3, 7, 4],
+               '20': [-3, 8, 5],
+               '21': [-3, 9, 6],
+               '22': [-4, 8, 4],
+               '23': [-4, 9, 5],
+               '24': [-4, 10, 6],
+               '25': [-5, 10, 5],
+               '26': [-5, 11, 6],
+               '27': [-6, 12, 6]}
+
+abaka_dict = {'1': [0, 10],
+              '2': [0, 20],
+              '3': [0, 30],
+              '4': [0, 40],
+              '5': [0, 50],
+              '6': [0, 60],
+              '7': [0, 10],
+              '8': [0, 20],
+              '9': [0, 30],
+              '10': [0, 40],
+              '11': [0, 50],
+              '12': [0, 60],
+              '13': [0, 10],
+              '14': [0, 20],
+              '15': [0, 30],
+              '16': [0, 40],
+              '17': [0, 50],
+              '18': [0, 60],
+              '19': [0, 10],
+              '20': [0, 20],
+              '21': [0, 30],
+              '22': [0, 40],
+              '23': [0, 50],
+              '24': [0, 60],
+              '25': [0, 10],
+              '26': [0, 20],
+              '27': [0, 30],
+              '28': [0, 40],
+              '29': [0, 50],
+              '30': [0, 60]}
+
+bonus_dict = {'1': [0, 4, 5, 6, 6, 9, 12],
+              '2': [0, 4, 5, 6, 6, 9, 12],
+              '3': [0, 4, 5, 6, 6, 9, 12],
+              '4': [0, 4, 5, 6, 6, 9, 12],
+              '5': [0, 4, 5, 6, 6, 9, 12],
+              '6': [0, 4, 5, 6, 6, 9, 12],
+              '7': [0, 4, 5, 6, 6, 9, 12],
+              '8': [0, 4, 5, 6, 6, 9, 12],
+              '9': [0, 5, 6, 7, 8, 12, 16],
+              '10': [0, 5, 6, 7, 8, 12, 16],
+              '11': [0, 5, 6, 7, 8, 12, 16],
+              '12': [0, 5, 6, 7, 8, 12, 16],
+              '13': [0, 5, 6, 7, 8, 12, 16],
+              '14': [0, 5, 6, 7, 8, 12, 16],
+              '15': [0, 5, 6, 7, 8, 12, 16],
+              '16': [0, 5, 6, 7, 8, 12, 16],
+              '17': [0, 6, 7, 8, 10, 15, 20],
+              '18': [0, 6, 7, 8, 10, 15, 20],
+              '19': [0, 6, 7, 8, 10, 15, 20],
+              '20': [0, 6, 7, 8, 10, 15, 20],
+              '21': [0, 6, 7, 8, 10, 15, 20],
+              '22': [0, 6, 7, 8, 10, 15, 20],
+              '23': [0, 6, 7, 8, 10, 15, 20],
+              '24': [0, 6, 7, 8, 10, 15, 20]}
